ad194e703baf229d93e495941dce56e86a948747
romadiansky <letsshitrainbows@gmail.com>
2018-09-27 23:50:08 +0000

    counttteeeddd down!

--- finding_waldo_refactored.js
+++ finding_waldo_refactored.js
@@ -11,3 +11,22 @@ function actionWhenFound(index) {
 }
 
 findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
+
+
+//here is it, refactored
+
+function findWaldo(arr) {
+  arr.forEach(findName);
+}
+
+function findName(name, i) {
+    if (name === "Waldo") {
+      actionWhenFound(i);   // execute callback
+    }
+  }
+
+function actionWhenFound(index) {
+  console.log("Found him at index: ", index);
+}
+
+findWaldo(["Alice", "Bob", "Waldo", "Winston"]);
